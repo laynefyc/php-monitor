4716639b1a069de955cd58819af5c31c62430723
xiongfanglei <xiongfanglei@xiaozhu.com>
2020-01-12 01:14:07 +0800

    if $_GET is null,301 redirect

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 use pm\common\Router;
 
+if(empty($_GET)){
+    header("Location: /public/",TRUE,301);
+}
 include 'vendor/autoload.php';
 $config = require('src/config/config.php');
 (new Router($config))->run();
